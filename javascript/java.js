cddd2bd5a577c2d2a63352eaec1c569f91a91a29
AlexRA95 <114920823+AlexRA95@users.noreply.github.com>
2023-02-27 17:49:41 +0100

    Add files via upload

---
+++ javascript/java.js
@@ -0,0 +1,17 @@
+
+
+function newsletter(){
+    
+    Swal.fire({
+        customClass: {
+            confirmButton: 'btn btn-success',
+        },
+        icon: 'success',
+        
+        title: '¡Gracias por suscribirse!',
+
+        showConfirmButton: true,
+            
+      })
+
+}
